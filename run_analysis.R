ba7b7b415f8af41a764b50fe623829798dad3b49
fermatr5 <52393522+fermatr5@users.noreply.github.com>
2020-05-10 19:25:46 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -2,11 +2,11 @@
 library(dplyr)
 
 #import and load data
-setwd("/Desktop/UCI HAR Dataset/train")
+setwd("C:/Users/rboll/Documents/UCI HAR Dataset/train")
 training.subject <- read.csv("subject_train.txt", sep = "", header = FALSE)
 training.x <- read.csv("X_train.txt", sep = "", header = FALSE)
 training.y <- read.csv("y_train.txt", sep = "", header = FALSE)
-setwd("/Desktop/UCI HAR Dataset/test")
+setwd("C:/Users/rboll/Documents/UCI HAR Dataset/test")
 testing.subject <-  read.csv("subject_test.txt", sep = "", header = FALSE)
 testing.x <- read.csv("X_test.txt", sep = "", header = FALSE)
 testing.y <- read.csv("y_test.txt", sep = "", header = FALSE)
@@ -16,30 +16,34 @@ training <- data.frame(training.subject, training.y, training.x)
 testing <- data.frame(testing.subject, testing.y, testing.x)
 data <- rbind(training, testing)
 
+#get mean and std information
+apply(training, 1, mean)
+#apply(training, 1, std)
+apply(testing, 1, mean)
+#apply(testing, 1, std)
+
+#change labels
+data$V1[data$V1 == 1] <-"WALKING"
+data$V1[data$V1 == 2] <-"WALKING UPSTAIRS"
+data$V1[data$V1 == 3] <- "WALKING DOWNSTAIRS"
+data$V1[data$V1 == 4] <- "SITTING"
+data$V1[data$V1 == 5] <- "STANDING"
+data$V1[data$V1 == 6] <- "LAYING"
+
 #read lables in features dataset
-setwd("/Desktop/UCI HAR Dataset/")
+setwd("C:/Users/rboll/Documents/UCI HAR Dataset")
 features <- read.csv("features.txt", sep= "", header=FALSE)
-#convert to vector
-column.names <- as.vector(features[, 2])
+#reformat to get activity and subject headers
+feature <- rbind(features[,c(1,2)], matrix(c(562,"activity", 563, "subject"), nrow = 2, byrow = TRUE))
 #add column labels
-colnames(data) <- c("subject_id", "activity_labels", column.names)
-
-#select appropriate columns needed
-data <- select(data, contains("subject"), contains("label"), contains("mean"), contains("std"), -contains("freq"), -contains("angle"))
-
-#read activity labels
-activity.labels <- read.csv("activity_labels.txt", sep = "", header = FALSE)
-#add activity lables instead of codes
-data$activity_labels <- as.character(activity.labels[match(data$activity_labels, activity.labels$V1), 'V2'])
-
-#clean up data by removing extraneous characters
-setnames(data, colnames(data), gsub("\\(\\)", "", colnames(data)))
-setnames(data, colnames(data), gsub("-", "_", colnames(data)))
-setnames(data, colnames(data), gsub("BodyBody", "Body", colnames(data)))
+colnames(data) <- feature[,2]
 
 #group data by subject and activity, calculate mean for the measurements
-data.summary <- data %>% group_by(subject_id, activity_labels) %>% summarise_each(funs(mean))
+activitymean <- aggregate(data$activity, data, mean)
+subjectmean <- aggregate(activitymean$subject, activitymean, mean)
+data.summary <- subjectmean[,c(564,565)]
 
 #write table
-write.table(data.summary, file = "run_data_summary.txt", row.name = FALSE)
+write.table(data.summary, file = "data_table.txt", row.name = FALSE)
+
 
